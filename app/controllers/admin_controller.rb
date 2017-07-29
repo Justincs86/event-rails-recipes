0d685271f239673b739f9319d3aff58dd01bbb05
justin <justinng@Justins-MacBook-Air.local>
2017-07-29 18:20:12 +0800

    implement role:admin

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -2,7 +2,18 @@ class AdminController < ApplicationController
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :require_admin!
 
   layout "admin"
+
+  protected
+
+  def require_admin!
+    if current_user.role != "admin"
+      flash[:alert] = "You are not admin!"
+      redirect_to root_path
+    end
+  end
+
 
 end
